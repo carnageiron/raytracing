dfba09cc239f601e078cc62a8652fa73b567e8d1
Aditya Srivastava <98521803+carnageiron@users.noreply.github.com>
2023-08-01 21:49:17 +0530

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -38,6 +38,12 @@ class vec3{
     double length_squared() const {
       return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
+    inline static vec3 random(){
+      return vec3(random_double(), random_double(), random_double());
+    }
+    inline static vec3 random(double min, double max){
+      return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+    }
 public:
     double e[3];
 };
@@ -82,4 +88,14 @@ inline double dot(const vec3 &u, const vec3 &v){
 }
 inline vec3 unit_vector(vec3 v){
   return v/v.length();
+}
+inline vec3 random_in_unit_sphere(){
+  while(true){
+    auto p = vec3::random(-1,1);
+    if (p.length_squared() >=1) continue;
+    return p;
+ }
+}
+vec3 random_unit_vector() {
+  return unit_vector(random_in_unit_sphere());
 }

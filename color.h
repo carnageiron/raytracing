8b624fb45d93fd13801b5c3addefd04787aa036f
Aditya Srivastava <98521803+carnageiron@users.noreply.github.com>
2023-08-01 21:36:20 +0530

    Update color.h

--- color.h
+++ color.h
@@ -5,9 +5,19 @@
 #include "vec3.h"
 
 void write_color(std::ostream &out, color pixel_color){
+
+  auto r = pixel_color.x();
+  auto g = pixel_color.y();
+  auto b = pixel_color.z();
+
+  //divide color,gamma correction
+  auto scale = 1.0 / sample_per_pixel;
+  r = sqrt(scale * r);
+  g = sqrt(scale * g);
+  b = sqrt(scale * b);
   //translated values of each color component [0,255]
   out << static_cast<int>(255.999 * pixel_color.x())<< ' '
-  out << static_cast<int>(255.999 * pixel_color.y())<< ' '
-  out << static_cast<int>(255.999 * pixel_color.z())<< '\n';
+      << static_cast<int>(255.999 * pixel_color.y())<< ' '
+      << static_cast<int>(255.999 * pixel_color.z())<< '\n';
 }
 #endif

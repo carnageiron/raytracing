f50121c4eb81d91d9ca349f37d5c3ccc9dd7b4c7
Aditya Srivastava <98521803+carnageiron@users.noreply.github.com>
2023-08-02 21:29:00 +0530

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -46,6 +46,14 @@ class vec3{
     }
 public:
     double e[3];
+
+
+bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+}
+
 };
 //type aliases for vec3
 using point3 = vec3; //3d point 

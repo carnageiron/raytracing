a6fbc3e6d990f82b2fa0ab8ebbac35f20d3575d5
Aditya Srivastava <98521803+carnageiron@users.noreply.github.com>
2023-08-02 21:22:27 +0530

    Update material.h

--- material.h
+++ material.h
@@ -12,4 +12,24 @@ class material {
     ) const = 0;
 };
 
+
+class lambertian : public material{
+  public:
+    lambertian(const color& a) : albedo(a) {}
+    virtual bool scatter(
+      const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+      auto scatter_directiion = rec.normal + random_unit_vector();
+
+      if(scatter_direction.near_zero())
+        scatter_direction = rec.normal
+        scattered = ray(rec.p, scatter.direction);
+        attenuation = albedo
+        return true;
+    }
+  public:
+    color albedo;
+};
+          
+
 #endif

a862a3528e5282661b670f69429bfe197c7f97e2
Aditya Srivastava <98521803+carnageiron@users.noreply.github.com>
2023-08-05 19:13:22 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include "hittable.h"
 #include "sphere.h"
 #include "camera.h"
+#include "material.h"
 
 
 color ray_color (const ray& r const hittable& world, int depth) {
@@ -35,8 +36,17 @@ int main(){
 
   //world
   hittable_list world;
-  world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-  world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+
 
   //camera
   camera cam;
